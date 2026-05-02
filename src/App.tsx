be8960a3ec45ebbba375ec148e65267f71ddcfc1
kmnhngyn <45228418+kmnhngyn@users.noreply.github.com>
2026-05-02 11:04:07 +0200

    chore: initial commit with vite, react and tailwind setuo

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          fontend
+        </div>
+        
+      </section>
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
